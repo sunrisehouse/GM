6531365635925fbef5f79fffad140d60c47e5e48
HanJungwoo1102 <ajtwlswjddnv1102@gmail.com>
2022-05-26 06:04:11 +0900

    feat:change the theme

--- index.js
+++ index.js
@@ -6,6 +6,12 @@ class Circle {
         this.color = color;
     }
 
+    renderEle = (ele) => {
+        ele.setAttribute('cx', this.x);
+        ele.setAttribute('cy', this.y);
+        ele.setAttribute('r', this.radius);
+    }
+
     render = (canvasContext) => {
         canvasContext.beginPath();
         canvasContext.arc(this.x, this.y, this.radius, 0, Math.PI * 2, true);
@@ -15,43 +21,115 @@ class Circle {
     };
 }
 
+class ImageObject {
+    constructor(x, y, width, height, imageUrl) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.image = new Image();
+        this.image.src = imageUrl;
+    }
+
+    render = (canvasContext) => {
+        canvasContext.drawImage(this.image, this.x, this.y, this.width, this.height);
+    };
+}
+
 class Scene {
     palletes = [
         {sunColor: '#f4eb99', skyColor: '#ffffff'}
     ];
     SUN_MIN_Y = 100;
 
-    constructor() {
+    constructor(initialWidth, initialHeight, sunEle) {
+        this.sunEle = sunEle;
         this.sun = new Circle(100, this.SUN_MIN_Y, 25, this.palletes[0].sunColor);
-        this.sunVelocity = { x: 0, y: 0.1 };
+        this.sunVelocity = { x: 0, y: 0 };
+        this.traveler = this.makeCarTraveler(initialWidth, initialHeight);
+        this.travelerVelocity = { x:-0.2, y:0 };
     }
 
-    render = (canvasContext, delta) => {
+    makeCarTraveler = (width, height) => {
+        return new ImageObject(width - 60, height - 114, 200, 200, './resources/red-car.png');
+    };
+
+    render = (canvasContext, delta, onSunArrivedOnGround) => {
         if (this.sun.y > canvasContext.canvas.height && this.sunVelocity.y > 0) {
-            this.sunVelocity = { x: 0, y: -0.1 };
+            this.sunVelocity = { x: 0, y: -0.2 };
+            this.traveler = this.makeCarTraveler(canvasContext.canvas.width, canvasContext.canvas.height);
+            onSunArrivedOnGround();
         }
         else if (this.sun.y < this.SUN_MIN_Y && this.sunVelocity.y < 0) {
-            this.sunVelocity = { x: 0, y: 0.1 };
+            this.sunVelocity = { x: 0, y: 0 };
         }
         this.sun.x += this.sunVelocity.x * delta;
         this.sun.y += this.sunVelocity.y * delta;
+
+        if (this.traveler && this.traveler.x < -150) {
+            this.sunVelocity = {x:0, y:0.2};
+        }
+        if (this.traveler) {
+            this.traveler.x += this.travelerVelocity.x * delta;
+            this.traveler.y += this.travelerVelocity.y * delta;
+        }
         
-        this.sun.render(canvasContext);
+        this.sun.renderEle(this.sunEle);
+        if (this.traveler) this.traveler.render(canvasContext);
     };
 }
 
 class Page {
+    palletes = [
+        {
+            mainColor1: '#f8efc7',
+            mainColor2: '#f4eb99',
+            mainColor3: '#c9db5f',
+            mainColor4: '#65b02c',
+            mainColor5: '#15902d',
+            primaryFontColor1: '#dddddd',
+            primaryFontColor2: '#666666',
+            primaryFontColor3: '#15902d',
+            sunColor: '#f4eb99',
+            groundColor: '#c9db5f',
+        },
+        {
+            mainColor1: '#FFA400',
+            mainColor2: '#D98C00',
+            mainColor3: '#0F00FF',
+            mainColor4: '#09009B',
+            mainColor5: '#000000',
+            primaryFontColor1: '#eeeeee',
+            primaryFontColor2: '#cccccc',
+            primaryFontColor3: '#09009B',
+            sunColor: '#ffa400',
+            groundColor: '#0F00FF',
+        },
+        {
+            mainColor1: '#E9E5D6',
+            mainColor2: '#ACB992',
+            mainColor3: '#464E2E',
+            mainColor4: '#362706',
+            mainColor5: '#362706',
+            primaryFontColor1: '#dddddd',
+            primaryFontColor2: '#666666',
+            primaryFontColor3: '#15902d',
+            sunColor: '#E83A14',
+            groundColor: '#064635',
+        },
+    ]
     constructor() {
         this.sky = document.getElementById('sky-layer');
         this.canvasEle = document.getElementById('scene');
+        this.scene = new Scene(this.sky.clientWidth, this.sky.clientHeight, document.getElementById('sun'));
+        this.currentPalletIdx = 0;
+
         this.matchCanvasToSky();
         this.canvasContext = this.canvasEle.getContext('2d');
         window.addEventListener('resize', this.onResizeCanvas, false);
 
-        this.scene = new Scene()
         this.prevTime = 0;
-
-        this.render(0)
+        this.render(0);
     }
 
     onResizeCanvas = () => {
@@ -62,12 +140,27 @@ class Page {
         this.canvasEle.width = this.sky.clientWidth;
         this.canvasEle.height = this.sky.clientHeight;
     };
+    
+    onSunArrivedOnGround = () => {
+        this.currentPalletIdx += 1;
+        if (this.currentPalletIdx == this.palletes.length) this.currentPalletIdx = 0;
+        document.documentElement.style.setProperty("--main-color-1", this.palletes[this.currentPalletIdx].mainColor1);
+        document.documentElement.style.setProperty("--main-color-2", this.palletes[this.currentPalletIdx].mainColor2);
+        document.documentElement.style.setProperty("--main-color-3", this.palletes[this.currentPalletIdx].mainColor3);
+        document.documentElement.style.setProperty("--main-color-4", this.palletes[this.currentPalletIdx].mainColor4);
+        document.documentElement.style.setProperty("--main-color-5", this.palletes[this.currentPalletIdx].mainColor5);
+        document.documentElement.style.setProperty("--primary-font-color-1", this.palletes[this.currentPalletIdx].primaryFontColor1);
+        document.documentElement.style.setProperty("--primary-font-color-2", this.palletes[this.currentPalletIdx].primaryFontColor2);
+        document.documentElement.style.setProperty("--primary-font-color-3", this.palletes[this.currentPalletIdx].primaryFontColor3);
+        document.documentElement.style.setProperty("--sun-color", this.palletes[this.currentPalletIdx].sunColor);
+        document.documentElement.style.setProperty("--ground-color", this.palletes[this.currentPalletIdx].groundColor);
+    }
 
     render = (time) => {
         this.canvasContext.clearRect(0, 0, this.canvasEle.width, this.canvasEle.height);
         
         const delta = time - this.prevTime;
-        this.scene.render(this.canvasContext, delta);
+        this.scene.render(this.canvasContext, delta, this.onSunArrivedOnGround);
         this.prevTime = time;
 
         window.requestAnimationFrame(this.render);
